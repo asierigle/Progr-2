c3b2bd6bb89535cd6f244b38def9182abef67d04
asierigle <asier.iglesias@yahoo.es>
2015-09-16 00:12:10 +0200

    a

---
+++ Progr 2/Main.cpp
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char* argv){
+	char* a = "Asier";
+	int length(); {
+	return strlen(a);
+	}
+	struct time{
+		int days;
+		int hours;
+		int mimutes;
+		int seconds;
+		if (minutes > 60){
+			hours + 1;
+		}
+		if (hours > 24){
+			days + 1;
+		}
+		~time(){}
+	};
+	int Addseconds(int seconds){
+	
+	}
+	//printf(length);
+	getchar();
+}
